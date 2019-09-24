d2431d8ab4d63bc0b274e74a4c45546fd8cc59ef
Williamcs1400 <williamcs1400@gmail.com>
2019-09-24 16:47:11 -0300

    arrumando umas bobagens

--- Trab-1/ClasseJogo.h
+++ Trab-1/ClasseJogo.h
@@ -33,7 +33,7 @@ Jogo::Jogo(){               //Metodo construtor da classe
     cin >> this->codigo;
 
     //Ler o campeonato
-    cout << "Digite o numero correspondente a competicao: " << endl;
+    cout << endl << "Digite o numero correspondente a competicao: " << endl;
     cout << "   (1) - Campeonato Brasileiro;" << endl;
     cout << "   (2) - Copa do Brasil;" << endl;
     cout << "   (3) - Estaduais;" << endl;
@@ -56,7 +56,7 @@ Jogo::Jogo(){               //Metodo construtor da classe
 
     //Ler a rodada do jogo se for pontos corridos ou fase se for copa
     if((this->campeonato == 1) || (this->campeonato  == 3)){
-        cout << "Digite o numero da rodada da partida";
+        cout << "Digite o numero da rodada da partida: ";
         cin >> this->rodada_fase;
     }
     else if((this->campeonato == 2) || (this->campeonato == 4) || (this->campeonato == 5)){
